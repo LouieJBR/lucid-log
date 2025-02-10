9b8dacd541ddd85128f2830e5ecf78b4bf9fe671
Louie Brown <devlouiebrown@outlook.com>
2025-02-10 16:37:10 +0000

    successfully wrote and saved a dream

--- lucid-log-frontend/src/app/app.module.ts
+++ lucid-log-frontend/src/app/app.module.ts
@@ -3,14 +3,26 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AuthModule } from '@auth0/auth0-angular';
+import { LoginComponent } from './login/login.component';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    AuthModule.forRoot({
+      domain: 'dev-m1vbm7mjkcjugfu4.uk.auth0.com',
+      clientId: 'FoaAMzt1kISLkfPu5OqO2eAfcd6xdnk2',
+      authorizationParams: {
+        redirect_uri: window.location.origin
+      }
+    })
   ],
   providers: [],
   bootstrap: [AppComponent]
