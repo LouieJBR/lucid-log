0386fec00d60e02ced74ccc446ea0a1738ac0f24
Louie Brown <devlouiebrown@outlook.com>
2025-02-14 11:58:12 +0000

    fixing fetch

--- lucid-log-frontend/src/app/service/user.service.ts
+++ lucid-log-frontend/src/app/service/user.service.ts
@@ -2,12 +2,13 @@ import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { UserProfile } from '../model/user.model';
+import { environment } from '../../environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class UserService {
-  private apiUrl = 'http://localhost:8080/api/auth/profile';
+  private apiUrl = environment.apiUrl + '/auth/profile';
 
   constructor(private http: HttpClient) {}
 
